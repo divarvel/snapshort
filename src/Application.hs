aaee466547e89ff71e5594fd9ce537c7909ab706
Clement Delafargue <clement@delafargue.name>
2014-03-09 00:07:30 +0100

    Stuff
    Use Text for Link
    Remove useless auth-related code + snaplets
    Add HTTP basic auth for unsafe endpoints + GET /urls/

--- src/Application.hs
+++ src/Application.hs
@@ -10,23 +10,14 @@ module Application where
 import Control.Lens
 import Snap (get)
 import Snap.Snaplet
-import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
-import Snap.Snaplet.Session
 import Snap.Snaplet.PostgresqlSimple
 
 ------------------------------------------------------------------------------
 data App = App
     { _pg :: Snaplet Postgres
-    , _heist :: Snaplet (Heist App)
-    , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
     }
 
 makeLenses ''App
-
-instance HasHeist App where
-    heistLens = subSnaplet heist
 
 instance HasPostgres (Handler b App) where
     getPostgresState = with pg get
--- src/Site.hs
+++ src/Site.hs
@@ -14,9 +14,10 @@ import           Control.Applicative
 import           Control.Monad
 import           Data.Aeson
 import           Data.Aeson.TH
-import           Data.ByteString (ByteString)
-import           Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString as BS
+import           Data.ByteString.Char8 (unpack, split)
 import           Data.ByteString.Lazy (fromStrict)
+import qualified Data.ByteString.Base64 as B64
 import           Data.Maybe (listToMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
@@ -36,10 +37,14 @@ import qualified Heist.Interpreted as I
 ------------------------------------------------------------------------------
 import           Application
 
+
+creds :: (BS.ByteString, BS.ByteString)
+creds = ("IbNPztMgTO", "wy1a3da1A4")
+
 data Link = Link
     { link_id :: UUID
-    , code :: ByteString
-    , long_url :: ByteString
+    , code :: T.Text
+    , long_url :: T.Text
     , hits :: Integer
     } deriving (Show, Eq)
 
@@ -57,28 +62,38 @@ instance FromRow Link where
     fromRow = Link <$> field <*> field <*> field <*> field
 
 
+authenticatedAction :: Handler App App () -> Handler App App ()
+authenticatedAction a = do
+    authData <- getBasicAuth
+    case authData of
+        Just creds -> a
+        Just _ -> do
+            modifyResponse $ setResponseCode 403
+        _ -> do
+            modifyResponse $ setResponseCode 401
+
+
 createNewLink :: Handler App App ()
-createNewLink = do
-  code <- getPostParam "code"
-  long_url <- getPostParam "long_url"
+createNewLink = authenticatedAction $ do
+  code <- fmap (fmap TE.decodeUtf8) $ getPostParam "code"
+  long_url <- fmap (fmap TE.decodeUtf8) $ getPostParam "long_url"
   uuid <- liftIO nextRandom
   newLink <- execute "INSERT INTO url VALUES (?, ?, ?, ?)" (uuid, code, long_url, (0 :: Integer))
   redirect "/"
 
 getAllLinks :: Handler App App ()
-getAllLinks = do
+getAllLinks = authenticatedAction $ do
   allLinks <- query_ "SELECT * FROM url"
   writeLBS $ encode (allLinks :: [Link])
-
 
 fetchLinkByUUID :: UUID -> Handler App App (Maybe Link)
 fetchLinkByUUID u = listToMaybe <$> query "select * from url where url_id = ?" (Only u)
 
-fetchLinkByCode :: ByteString -> Handler App App (Maybe Link)
+fetchLinkByCode :: BS.ByteString -> Handler App App (Maybe Link)
 fetchLinkByCode t = listToMaybe <$> query "select * from url where code = ?" (Only t)
 
-uuidParam :: Handler App App (Maybe ByteString) -> Handler App App (Maybe UUID)
-uuidParam = fmap (fromByteString . fromStrict =<<)
+uuidParam :: Handler App App (Maybe BS.ByteString) -> Handler App App (Maybe UUID)
+uuidParam = fmap (fromASCIIBytes =<<)
 
 getLink :: Handler App App ()
 getLink = do
@@ -92,63 +107,27 @@ getLink = do
 redirectLink :: Handler App App ()
 redirectLink = do
     code <- getParam "code"
-    link <- maybe (return Nothing) fetchLinkByCode (code :: Maybe ByteString)
+    link <- maybe (return Nothing) fetchLinkByCode (code :: Maybe BS.ByteString)
     maybe
         (modifyResponse $ setResponseCode 404)
-        (redirect . long_url)
+        (redirect . TE.encodeUtf8 . long_url)
         (link :: Maybe Link)
 
 deleteLink :: Handler App App ()
-deleteLink = do
+deleteLink = authenticatedAction $ do
     id <- getParam "id"
     _ <- execute "DELETE FROM url WHERE url_id = ?" (Only id)
     return ()
 
-------------------------------------------------------------------------------
--- | Render login form
-handleLogin :: Maybe T.Text -> Handler App (AuthManager App) ()
-handleLogin authError = heistLocal (I.bindSplices errs) $ render "login"
-  where
-    errs = maybe noSplices splice authError
-    splice err = "loginError" ## I.textSplice err
-
-
-------------------------------------------------------------------------------
--- | Handle login submit
-handleLoginSubmit :: Handler App (AuthManager App) ()
-handleLoginSubmit =
-    loginUser "login" "password" Nothing
-              (\_ -> handleLogin err) (redirect "/")
-  where
-    err = Just "Unknown user or password"
-
-
-------------------------------------------------------------------------------
--- | Logs out and redirects the user to the site index.
-handleLogout :: Handler App (AuthManager App) ()
-handleLogout = logout >> redirect "/"
-
-
-------------------------------------------------------------------------------
--- | Handle new user form submit
-handleNewUser :: Handler App (AuthManager App) ()
-handleNewUser = method GET handleForm <|> method POST handleFormSubmit
-  where
-    handleForm = render "new_user"
-    handleFormSubmit = registerUser "login" "password" >> redirect "/"
-
 
 ------------------------------------------------------------------------------
 -- | The application's routes.
-routes :: [(ByteString, Handler App App ())]
-routes = [ ("/login",      with auth handleLoginSubmit)
-         , ("/logout",     with auth handleLogout)
-         , ("/new_user",   with auth handleNewUser)
-         , ("/urls",       method GET getAllLinks)
+routes :: [(BS.ByteString, Handler App App ())]
+routes = [ ("/urls",       method GET getAllLinks)
          , ("/urls",       method POST createNewLink)
          , ("/urls/:id",   method GET getLink)
          , ("/urls/:id",   method DELETE deleteLink)
-         , ("",          serveDirectory "static")
+         , ("/:code",      method GET redirectLink)
          ]
 
 
@@ -157,16 +136,24 @@ routes = [ ("/login",      with auth handleLoginSubmit)
 app :: SnapletInit App App
 app = makeSnaplet "app" "An snaplet example application." Nothing $ do
     pg <- nestSnaplet "pg" pg pgsInit
-    h <- nestSnaplet "" heist $ heistInit "templates"
-    s <- nestSnaplet "sess" sess $
-           initCookieSessionManager "site_key.txt" "sess" (Just 3600)
-
-    -- NOTE: We're using initJsonFileAuthManager here because it's easy and
-    -- doesn't require any kind of database server to run.  In practice,
-    -- you'll probably want to change this to a more robust auth backend.
-    a <- nestSnaplet "auth" auth $
-           initJsonFileAuthManager defAuthSettings sess "users.json"
     addRoutes routes
-    addAuthSplices h auth
-    return $ App pg h s a
+    return $ App pg
 
+
+parseBasicAuth :: BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
+parseBasicAuth h = let
+    (authScheme, encoded) = BS.splitAt 6 h
+    in
+        if authScheme == "Basic " then
+            let
+                decoded = B64.decode encoded
+                components = fmap (split ':') decoded
+                tup c = case c of
+                    (x:y:_) -> Just (x, y)
+                    _ -> Nothing
+            in (either (const Nothing) Just components) >>= tup
+        else
+            Nothing
+
+getBasicAuth :: MonadSnap m => m (Maybe (BS.ByteString, BS.ByteString))
+getBasicAuth = withRequest (\r -> return $ getHeader "Authorization" r >>= parseBasicAuth)
